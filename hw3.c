6e33da5ed116e4b0772d5808e913b967fd2ed464
mrjbar <jabari.lofton@sjsu.edu>
2015-02-23 15:46:07 -0800

    Added print function
    The print function will now print out events with a timestamp.

--- hw3.c
+++ hw3.c
@@ -10,6 +10,7 @@ pthread_mutex_t queueMutex;
 pthread_mutex_t secOneMutex;
 pthread_mutex_t secTwoMutex;
 pthread_mutex_t secThreeMutex;
+pthread_mutex_t printMutex;
 sem_t removeAlarm;
 
 typedef enum{GS, RS, EE} studentType;
@@ -19,8 +20,9 @@ typedef enum{GS, RS, EE} studentType;
 #define COUNT 1
 
 int ID_BASE = 100;
+int firstPrint = 1;
 int first_Section[max_in_Section];
-int secSecCounter = 0;
+int firstSecCounter = 0;
 int second_Section[max_in_Section];
 int third_Section[max_in_Section];
 int GS_front = -1, RS_front = -1, EE_front = -1;
@@ -38,71 +40,24 @@ time_t startTime;
  };	typedef struct Student_struct Student;
 
 struct info* Students;
-
 Student GS_students[TotalStudents];
 Student RS_students[TotalStudents];
 Student EE_students[TotalStudents];
 
-void enroll(Student *potentialStudent)
-{
-	if(potentialStudent->type == GS)
-	{
-		add_to_Queue(potentialStudent, GS_students, &GS_front, &GS_rear);
-		if(GS_rear == GS_front)
-		{
-			// Signal to all processes that we are removing a student
-			//sem_post();
-			// Get the next student
-			remove_from_Queue(GS_students, &GS_front, &GS_rear);
-			// Sleep from 1 to 2 seconds
-			sleep(rand()*2);
-			// Add the student to the section if the section
-			// is open
-		}
-	}
-
-	pthread_mutex_lock(&secOneMutex);
-	if(potentialStudent->section_pref == 1)
-	{
-		if ((sizeof(second_Section) / 4) <= max_in_Section)
-		{
-			second_Section[secSecCounter] = potentialStudent->id;
-			secSecCounter++;
-		}	
-	}
-	pthread_mutex_unlock(&secOneMutex);
-}
-
-void *student(void *param)
-{
-	Student potentialStudent
-	potentialStudent.id = *((int *)param);
-	potentialStudent.section_pref = rand() % 3;
-	potentialStudent.max_wait_time = 10;
-
-	// Sleep the process from 0 - 120 seconds
-	int wakeUpTime = rand() % totalTime;
-	sleep(wakeUpTime);
-
-	// Get the current time and add it to the arrival time
-	time_t now;
-	time(&now);
-	potentialStudent.arrival_time = now;
-	
-	// Add the student to the queue
-	enroll(&potentialStudent);
-	return NULL;
-}
-
-void *stopwatch(void *param)
-{
-	time(&startTime);
-	sleep(120);
-	return NULL;
-}
+// Function Declarations
+Student remove_from_Queue(Student *queue, int *front, int *rear);
+void add_to_Queue(Student s, Student *queue, int *front, int *rear);
+void *student(void *param);
+void enroll(Student *potentialStudent);
+void *stopwatch(void *param);
+void print(char *event);
 
 int main()
 {
+	// Seed the random number generator with the time
+	srand(time(0));
+	time(&startTime);
+
 	int id = 0;
 	pthread_t stopWatchId;
 	pthread_attr_t stopWatchAttr;
@@ -123,6 +78,74 @@ int main()
 	return 0;
 }
 
+void *student(void *param)
+{
+	Student potentialStudent;
+	potentialStudent.id = *((int *)param);
+	potentialStudent.section_pref = rand() % 3;
+	potentialStudent.max_wait_time = 10;
+
+	// Sleep the process from 0 - 120 seconds
+	int wakeUpTime = rand() % totalTime;
+	char event[80];
+	sprintf(event, "Created student %d, now sleep for %d", potentialStudent.id, wakeUpTime);
+	print(event);
+	sleep(wakeUpTime);
+
+	// Get the current time and add it to the arrival time
+	time_t now;
+	time(&now);
+	potentialStudent.arrival_time = now;
+	
+	// Add the student to the queue
+	enroll(&potentialStudent);
+	return NULL;
+}
+
+void enroll(Student *potentialStudent)
+{
+	char event[80];
+	sprintf(event, "Student %d joined the queue", potentialStudent->id);
+	print(event);
+	if(potentialStudent->type == GS)
+	{
+		add_to_Queue(*potentialStudent, GS_students, &GS_front, &GS_rear);
+
+		if(GS_rear == GS_front)
+		{
+			// Signal to all processes that we are removing a student
+			//sem_post();
+			// Get the next student
+			remove_from_Queue(GS_students, &GS_front, &GS_rear);
+			// Sleep from 1 to 2 seconds
+			sleep(rand()*2);
+			// Add the student to the section if the section
+			// is open
+		}
+	}
+
+	pthread_mutex_lock(&secOneMutex);
+	if(potentialStudent->section_pref == 1)
+	{
+		if ((sizeof(first_Section) / 4) <= max_in_Section)
+		{
+			first_Section[firstSecCounter] = potentialStudent->id;
+			firstSecCounter++;
+		}	
+	}
+	pthread_mutex_unlock(&secOneMutex);
+	sprintf(event, "Student %d left the queue", potentialStudent->id);
+	print(event);
+}
+
+void *stopwatch(void *param)
+{
+	sleep(120);
+	char event[80];
+	sprintf(event, "Enrollment is over");
+	print(event);
+	return NULL;
+}
 void add_to_Queue(Student s, Student *queue, int *front, int *rear)
 {
 	pthread_mutex_lock(&queueMutex);
@@ -156,6 +179,38 @@ Student remove_from_Queue(Student *queue, int *front, int *rear)
 	return removed;
 }
 
-void display(){
+void print(char *event)
+{
+	time_t now;
+	time(&now);
+	double elapsed = difftime(now, startTime);
+	int min = 0;
+	int sec = (int) elapsed;
 
+	if( sec >= 60)
+	{
+		min++;
+		sec -= 60;
+	}
+
+	// Acquire the mutex lock to protect the printing
+	pthread_mutex_lock(&printMutex);
+	
+	if(firstPrint) 
+	{
+		printf("TIME | EVENT\n");
+		firstPrint = 0;
+	}
+
+	// Elapsed time.
+	printf("%1d:%02d | ", min, sec);
+
+	// Which student is currently being processed
+	//printf("%5d |", )
+
+	// What even occurred.
+	printf("%s\n", event);
+
+	// Release the mutex lock
+	pthread_mutex_unlock(&printMutex);
 }
